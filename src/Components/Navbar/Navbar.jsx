9ed2bc66dd4218c341f1383cc6c6dc3d0397253e
alaa <alaashouk0@gmail.com>
2026-05-24 14:32:07 +0300

    first commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+import Search_icon from '../../assets/search_icon.svg'
+import bell_icon from '../../assets/bell_icon.svg'
+import profile_img from '../../assets/profile_img.png'
+import caret_icon from '../../assets/caret_icon.svg'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className= 'navbar-left'>
+        <img src={logo} alt="Logo" />
+        <ul>
+            <li>Home</li>
+            <li>TV Shows</li>
+            <li>Movies</li>
+            <li>New & Popular</li>
+            <li>My List</li>
+            <li>Browse by Languages</li>
+        </ul>
+      </div>
+      <div className='navbar-right'>
+        <img src={Search_icon} alt="Search" className='icons'/>
+        <p>Children</p>
+        <img src={bell_icon} alt="Bell" className='icons'/>
+        <div className ="navbar-profile">
+          <img src={profile_img} alt="Profile"  className="profile"/>
+          <img src={caret_icon} alt="Caret" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/Components/TitileCards/TitileCards.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TitileCards.css'
+
+const TitileCards = () => {
+  return (
+    <div className='titleCards'>
+      
+    </div>
+  )
+}
+
+export default TitileCards

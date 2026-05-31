214cc25af27a9a9d7ca7e8777b60c54de98ab544
alaa <alaashouk0@gmail.com>
2026-05-31 14:48:22 +0300

    implement hero section

--- src/Components/TitileCards/TitileCards.jsx
+++ src/Components/TitileCards/TitileCards.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React ,{ useEffect , useRef } from 'react'
 import './TitileCards.css'
+import cards_data from "../../assets/cards/Cards_data";
 
 const TitileCards = () => {
+
+const cardsRef = useRef(); 
+
+const handleWheel = (e) => { 
+  e.preventDefault();
+  cardsRef.current.scrollLeft += e.deltaY;
+}
+ 
+useEffect(() => {
+  cardsRef.current.addEventListener("wheel", handleWheel); 
+},[] );
+
   return (
-    <div className='titleCards'>
-      
+    <>
+    <div className='titleCards'> 
+      <h2>popular on Netflix</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card , index) => {
+          return <div className="card" key={index}>
+           <img src={card.image} alt="" className='card-img' /> 
+           <p>{card.name}</p>
+          </div>
+        })}
+      </div>
     </div>
+      </>
   )
 }
 
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,9 +3,15 @@ import './Home.css'
 import Navbar from '../../Components/Navbar/Navbar'
 import hero_banner from '../../assets/hero_banner.jpg'
 import hero_Titel from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
+import TitileCards from '../../Components/TitileCards/TitileCards'
+import { LuBadgeInfo } from "react-icons/lu";
 
+ 
 function Home() {
   return (
+    <>
     <div className='home'>
       <Navbar />
       <div className="hero">
@@ -14,11 +20,30 @@ function Home() {
           <img src={hero_Titel} alt='title' className='caption-img' />
           <p>
             Discovering his ties to a secret ancient order , a young man living 
-             in modern Istanbul embarks on a quest to save the city form an immortal enemy
+            in modern Istanbul embarks on a quest to save the city form an immortal enemy
           </p>
+          <div className="hero-btns">
+            <button className="btn">
+              <img src={play_icon} alt='play' className='btn-icon' />play
+            </button>
+            <button className="btn btn-info dark-btn">
+              <LuBadgeInfo className='btn-icon' />
+              <span>More info</span>
+            </button>
+          </div>
+          <TitileCards />
         </div>
       </div>
     </div>
+    {/* //! more cards (Second Section ) */}
+        <div className="more-cards">
+        <TitileCards/>
+        <TitileCards/>
+        <TitileCards/> 
+       
+        
+      </div>
+      </>
   )
 }
 
